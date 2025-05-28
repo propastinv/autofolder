0667d2bd7e7011d83cb7b06a1b75a962d8df46f5
propastinv <Propastin.V@outlook.com>
2025-05-28 19:10:15 +0300

    set use_tls=False

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def create_folder_if_not_exists(mail, folder_name):
 def add_sieve_filter(email_address, folder_name):
     try:
         print(f"Connecting to ManageSieve on {SIEVE_HOST}:{SIEVE_PORT}...")
-        client = MANAGESIEVE(host=SIEVE_HOST, port=SIEVE_PORT, use_tls=True, keyfile=None, certfile=None)
+        client = MANAGESIEVE(host=SIEVE_HOST, port=SIEVE_PORT, use_tls=False, keyfile=None, certfile=None)
         print("Connected")
 
         print(f"Logging in as accounts with PLAIN LOGIN auth")
